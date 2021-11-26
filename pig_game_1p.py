c8edef64a1e0d8d0ed003ce59b0b539fd3a25ba7
Matthew Peyton <matpey0311@gmail.com>
2021-11-26 12:38:43 -0700

    couple fixes for the pig game

--- pig_game_1p.py
+++ pig_game_1p.py
@@ -22,9 +22,6 @@ class pig_game():
         else: self.current_score = 0
         return last_roll, self.current_score
 
-    def piggy_total(self):
-        return self.total
-
     def piggy_bank(self):
         return self.bank, self.total
 
@@ -36,13 +33,13 @@ class pig_game():
 if __name__ == "__main__":
     pig = pig_game()
     target_score = 100
-    while pig.piggy_total() < target_score:
+    while pig.piggy_bank()[1] < target_score:
         this_roll = pig.roll_handler()
         if pig.piggy_bank()[1] + this_roll[1] >= target_score:
             print('You rolled a ' + str(this_roll[0]))
             pig.end_turn(this_roll[1])
         elif this_roll[0] == 1:
-            input('You rolled a 1 and ended your turn.')
+            input('You rolled a 1 and ended your turn. Your existing total is ' + str(pig.piggy_bank()[1]))
             pig.end_turn(this_roll[1])
         else:
             rollq = input('You rolled a ' + str(this_roll[0]) + ' and now have ' + str(this_roll[1]) + ' points. Your existing total is ' + str(pig.piggy_bank()[1]) + '. Do you want to "roll" or "stay"?')
